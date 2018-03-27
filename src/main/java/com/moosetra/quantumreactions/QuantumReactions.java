ff8eb139fcd2a92786387914f1a7ada5f124a731
moosetra <devonwebb559@gmail.com>
2018-03-26 23:40:47 -0700

    Recommit of initial mod file

--- src/main/java/com/moosetra/quantumreactions/QuantumReactions.java
+++ src/main/java/com/moosetra/quantumreactions/QuantumReactions.java
@@ -1,5 +1,8 @@
 package com.moosetra.quantumreactions;
 
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "QuantumReactions", name = "Quantum Reactions", version = "1.12-0.1a")
 public class QuantumReactions {
 
 }
